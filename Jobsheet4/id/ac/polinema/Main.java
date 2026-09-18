a93843b1632f741c63ff935cc2bb0bb6f22b4f90
Pidddd <424f1d@gmail.com>
2026-09-19 00:46:13 +0700

    Tugas

--- Jobsheet4/id/ac/polinema/Main.java
+++ Jobsheet4/id/ac/polinema/Main.java
@@ -2,21 +2,23 @@ package Jobsheet4.id.ac.polinema;
 
 public class Main {
     public static void main(String[] args) {
-        Costumer costumer1 = new Costumer("Nadia", "0812-0000-0001");
-        Account acc1 = new Account("A001", costumer1, 500000);
-        acc1.withdraw(150000);
+        Costumer nadia = new Costumer("Nadia", "0812-0000-0001");
+        Account acc1 = new Account("A001", nadia, 500000);
+        Account acc2 = new Account("A002", nadia, 300000);
 
-        Costumer costumer2 = new Costumer("Sari", "0812-0000-0002");
-        Account acc2 = new Account("A002", costumer2, 200000);
+        Costumer sari = new Costumer("Sari", "0812-0000-0002");
+        Account acc3 = new Account("A003", sari, 200000);
 
         Bank bank = new Bank(10);
         bank.addAccount(acc1);
         bank.addAccount(acc2);
-        bank.printAllAccounts();
+        bank.addAccount(acc3);
 
-        Account found = bank.findAccount("A002");
-        if(found != null) {
-            found.printInfo();
+        Account[] nadiaAccounts = bank.findAccountsByOwner("Nadia");
+
+        System.out.println("Jumlah akun: " + nadiaAccounts.length);
+        for (int i = 0; i < nadiaAccounts.length; i++) {
+            nadiaAccounts[i].printInfo();
         }
     }
 }
